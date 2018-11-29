522d73f1206686f4eeca68f8fab753ec5063f6e6
Mansard <nmansard@laas.fr>
2018-11-29 15:57:33 +0100

    Cozmo model, continuous and discretize.

---
+++ cozmomodel.py
@@ -0,0 +1,211 @@
+'''
+We denote by:
+ - a,b the position in the plane
+ - theta the orientation of the body, c=cos(theta), s=sin(theta)
+ - v,w the forward velocity and angular velocity
+ - vr and vl the linear velocity of the caterpilar
+ - r the radius of the circle that the robot is following 
+ - delta the space between the caterpilars.
+
+Then 
+vr = -w(r-delta) ; vl = -w(r+delta)
+v = wr = (vr+vl)/2
+w = (vr-vl)/delta
+
+With the configuration a,b,c,s and the control u1=vr, u2=vl:
+adot = v cos(theta) = (vr+vl)c
+bdot = v sin(theta) = (vr+vl)s
+cdot = d/dt cos(theta) = -sin(theta) w = -s (vr-vl)/delta
+sdot = d/dt sin(theta) =  cos(theta) w =  c (vr-vl)/delta
+
+'''
+import numpy as np
+from discretization import VectorDiscretization
+
+# Because plt.pause raises a false warning (https://stackoverflow.com/questions/22873410/)
+import warnings
+warnings.filterwarnings("ignore",".*GUI is implemented.*")
+
+class Cozmo1(object):
+    '''
+    Velocity-controled model of Cozmo.
+    The main methods reset, step and render are used to roll-out Cozmo behaviours.
+    The class can be built with continuous or discretized state and control (continuous
+    by default).
+    '''
+    def __init__(self, discretize_x = False, discretize_u = False):
+        '''
+        Build the environment with or without discretize state and control.
+        The state is continous in theory, and the main functions randomState, dyn, cost and
+        display works with a continuous state and control. 
+        However, a discretization can be added with the proper arguments.
+        In that case, the stored state / control are int variables. Use self.decode_{x,u} and
+        self.encode_{x,u} to go from int to continuous (decode) and reverse (encode).
+        The reset, step and render methods are basically decoding state and control (if
+        need be), calling the respective randomState, dyn-cost, display methods, and
+        finally encoding state and control (if need be).
+        '''
+        # The state is stored as x_pos,y_pos,cos(theta),sin(theta), with c**2+s**2=1
+        # The control is the velocities of right and left tracks u = vr,vl.
+        self.xmax = np.array([ .2,  .2, 1., 1. ])       # Bounds of the state.
+        self.xmin = -self.xmax                          # Lower bounds of the state
+        self.umax = np.array([ .55, .55 ])              # Upper bound of control
+        self.umin = -self.umax                          # Lower bound of control
+        # Discretization of X and U
+        if discretize_u:
+            self.discretize_u = VectorDiscretization(2,vmax=self.umax,nsteps=11)
+            self.encode_u = self.discretize_u.c2i
+            self.decode_u = self.discretize_u.i2c
+        else:
+            self.discretize_u = None
+            self.encode_u = lambda u:u
+            self.decode_u = lambda u:u
+        if discretize_x:
+            self.discretize_x = VectorDiscretization(2,vmax=self.xmax,nsteps=10)
+            self.encode_x = self.discretize_x.c2i
+            self.decode_x = self.discretize_x.i2c
+        else:
+            self.discretize_x = None
+            self.encode_x = lambda x:x
+            self.decode_x = lambda x:x
+        # Model parameters
+        self.delta = .1                                 # Space between the two caterpillars
+        self.dt               = .1                      # Duration of one step(x,u)
+        self.integrationSteps = 10                      # Number of internal integration in one
+                                                        # step (of time dt/steps)
+        # Internal mutables
+        self._ax = None
+        self._plotobject_right = None; self._plotobject_left = None
+        self.expected_shape = [1,4]
+        self.reshape_x = lambda x: np.reshape(x,self.expected_shape) if self.discretize_x is None else lambda x:x
+        
+    @property
+    def nu(self): return 2 if self.discretize_u is None else self.discretize_u.nd
+    @property
+    def nx(self): return 4 if self.discretize_x is None else self.discretize_x.nd
+    
+    def render(self,sleep=.01,ion=True,newfig=False):
+        '''
+        This method internally calls self.display, but first recover the 
+        robot state and build / clean the figure display.
+        '''
+        import matplotlib.pyplot as plt
+        if ion: plt.ion()
+        else: plt.ioff()
+        if self._ax is None or len(self._ax.axes)==0:
+            self._ax = plt.figure() if newfig else plt.gcf()
+            self._plotobject_left = None
+            self._plotobject_right = None
+            if plt.axis() == (0,1,0,1):
+                plt.axis([ self.xmin[0]-2*sc,self.xmax[0]+2*sc,self.xmin[1]-2*sc,self.xmax[1]+2*sc])
+        else:
+            if self._plotobject_right is not None: self._plotobject_right.remove()
+            if self._plotobject_left is not None: self._plotobject_left.remove()
+        self._plotobject_left,self._plotobject_right = self.display(self.decode_x(self.x))
+        if sleep>0: plt.pause(sleep)
+    def reset(self,x=None):
+        '''
+        This method internally calls self.randomState() and stores the results. 
+        '''
+        x =  self.randomState() if x is None else np.reshape(self.decode_x(x),[self.nx])
+        self.x = self.encode_x(x)
+        return self.reshape_x(self.x)
+    def step(self,u):
+        '''
+        This method internally calls self.cost() and self.dyn() from internal state,
+        stores the results and return xnext,reward.
+        '''
+        if self.discretize_u is not None and isinstance(u,np.ndarray): u = u.flat[0]
+        self.u = u
+        u = self.decode_u(u)
+        if self.discretize_u is None: self.u = u = np.clip(u,self.umin,self.umax)
+        x = self.decode_x(self.x).copy()
+        for i in range(self.integrationSteps):
+            x += self.dyn(x,u)*self.dt / self.integrationSteps
+            x[2:4] /= np.linalg.norm(x[2:4])
+            #x = np.clip(x,self.xmin,self.xmax)
+        #assert(np.all(x<=self.xmax) and np.all(x>=self.xmin))
+        self.x = self.encode_x(x)
+        self.r = self.cost(x,u)
+        return self.reshape_x(self.x),self.r
+
+    # Internal methods corresponding to reset (randomState), step (cost and dyn) and
+    # render (display). They are all to be used with continuous state and control, and are
+    # read only (no change of internal state).
+    def randomState(self):
+        xy = np.random.rand(2)*(self.xmax[:2]-self.xmin[:2])+self.xmin[:2]
+        th = np.random.rand()*np.pi*2
+        return np.concatenate([xy,[np.cos(th), np.sin(th)]])
+    def cost(self,x,u):
+        '''
+        Const method returning the reward of making one step (positive = reward, negative = cost).
+        This method is called inside step(u).
+        '''
+        norm = np.linalg.norm
+        return -np.linalg.norm(x-[0,0,1,0])-0.1*np.linalg.norm(u)
+    def dyn(self,x,u):
+        '''
+        Const method: return f(x,u) = dx/dt 
+        This method is called inside step(u).
+        '''
+        a,b,c,s = x
+        vr,vl = u
+        v,w = (vr+vl)/2,(vr-vl)/self.delta
+        dx_dt  = np.array([ v*c,v*s, -s*w, c*w ])
+        return dx_dt
+    def display(self,x):
+        sc,delta = .1,self.delta
+        import matplotlib.pyplot as plt
+        a,b,c,s = x[:4]
+        refs = []
+        refs.append(plt.arrow(a-sc/2*c-delta*s,b-sc/2*s+delta*c,c*sc,s*sc,head_width=.03))
+        refs.append(plt.arrow(a-sc/2*c+delta*s,b-sc/2*s-delta*c,c*sc,s*sc,head_width=.03))
+        return refs
+
+class Cozmo2(Cozmo1):
+    '''
+    Second order cozmo model, where controls are caterpillar accelerations.
+    State x = [a,b,c,s,vr,vl], control u = [ar,al].
+    This second model is strongly built upon Cozmo1. Basically, only the
+    internal randomState, dyn and cost methods are changed.
+    '''
+    def __init__(self, discretize_x = False, discretize_u = False):
+        Cozmo1.__init__(self)
+        self.xmax = np.concatenate([self.xmax,self.umax])
+        self.xmin = np.concatenate([self.xmin,self.umin])
+        self.umax = np.array([ .5,.5 ])
+        self.umin = -self.umax
+        # Discretization of X and U
+        if discretize_u:
+            self.discretize_u = VectorDiscretization(2,vmax=self.umax,nsteps=5) #11
+            self.encode_u = self.discretize_u.c2i
+            self.decode_u = self.discretize_u.i2c
+        else:
+            self.discretize_u = None
+            self.encode_u = lambda u:u
+            self.decode_u = lambda u:u
+        assert(not discretize_x) # Not implemented, not sure that discretize state dim=5 is relevant.
+        self.discretize_x = None; self.decode_x = self.encode_x = lambda x:x
+        self.expected_shape = [1,6]
+        
+    @property
+    def nx(self): return 6
+        
+    def randomState(self):
+        x = Cozmo1.randomState(self)
+        v = np.random.rand(2)*(self.xmax[4:6]-self.xmin[4:6])+self.xmin[4:6]
+        return np.concatenate([x,v])
+    def dyn(self,x,u):
+        return np.concatenate([ Cozmo1.dyn(self,x=x[:4],u=x[4:6]),u ])
+    def cost(self,x,u):
+        norm = np.linalg.norm
+        return -norm(x[:4]-[0,0,1,0]) \
+            -0.01*norm(x[4:]) \
+            -0.1*norm(u)
+       
+    
+if __name__ == "__main__":
+    env = Cozmo1(discretize_u=False)
+    env.reset()
+    for i in range(100):
+        assert( np.all(env.reset()>env.xmin) and np.all(env.reset()<env.xmax))
---
+++ discretization.py
@@ -0,0 +1,62 @@
+'''
+Helper class to systematically convert a R^n vector of float into a 
+N^n vector of integers, and to a single integer.
+The main methods are flat2continuous and continuous2flat.
+Build it from the size of the real vector space, the bounds in each directions and
+the number of discretization steps.
+
+See also numpy.digitalize.
+'''
+
+import numpy as np
+from functools import reduce
+
+class VectorDiscretization:
+    '''
+    Convertion between continuous R^N bounded vectors and their discretization
+    as N^N bounded integer vectors and N*N integer.
+    '''
+    def __init__(self,nv,vmax=1.,vmin=None,nsteps=10):
+        self.nv = nv  # Dimension of the vector space
+        self.vmax = vmax if isinstance(vmax,np.ndarray) else np.array([vmax,]*nv)
+        self.vmin = vmin if isinstance(vmin,np.ndarray) else -self.vmax if vmin is None else np.array([vmin,]*nv)
+        self.nsteps = nsteps if isinstance(nsteps,np.ndarray) else np.array([nsteps,]*nv)
+        self.nd = reduce(lambda i,j:i*j,self.nsteps)
+        assert( self.nsteps.dtype == np.int32 or self.nsteps.dtype == np.int64)
+
+    def continuous2discrete(self,vc):
+        vc = np.clip(vc,self.vmin,self.vmax)
+        return ((vc-self.vmin)/(self.vmax-self.vmin)*self.nsteps).astype(self.nsteps.dtype)
+
+    def discrete2continuous(self,vd):
+        return (vd+.5)*(self.vmax-self.vmin)/self.nsteps+self.vmin
+
+    def flatten(self,vd):
+        '''change an array of int to a flat int.'''
+        assert( np.all(vd<self.nsteps) )
+        # Return (((v1)n2+v2)n3+v3)n4+...
+        # return reduce(lambda v1_n1,v2_n2: (v1_n1[0]*v2_n2[1]+v2_n2[0],v1_n1[1]*v2_n2[1]),
+        #               zip(vd,self.nsteps))[0]
+        res = 0
+        for v,n in zip(vd,self.nsteps):            res = res*n+v
+        return res
+    
+    def unflatten(self,i):
+        '''change a flat int into an array of int.'''
+        assert(i>=0 and i<self.nd)
+        # res,check = reduce(lambda res_ii,n: (res_ii[0]+[res_ii[1]%n],res_ii[1]/n),
+        #                    [([],i)]+self.nsteps.tolist())
+        res = []
+        for n in self.nsteps:
+            res.append(i%n)
+            i //= n
+        assert(0==i)
+        return np.array([ii for ii in reversed(res)])
+
+    def flat2continuous(self,i):
+        return self.discrete2continuous(self.unflatten(i))
+    def continuous2flat(self,vc):
+        return self.flatten(self.continuous2discrete(vc))
+
+    i2c=flat2continuous
+    c2i=continuous2flat
