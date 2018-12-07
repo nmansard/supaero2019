5da595358a2450509de0e4f8c03052095f987ca2
Mansard <nmansard@laas.fr>
2018-12-07 12:44:30 +0100

    IVIGIT TP1.:

---
+++ notebooks/gviewserver.py
@@ -0,0 +1,30 @@
+def GepettoViewerServer(windowName="python-pinocchio", sceneName="world", loadModel=False):
+    """
+    Init gepetto-viewer by loading the gui and creating a window.
+    """
+    import gepetto.corbaserver
+    try:
+        viewer = gepetto.corbaserver.Client()
+        gui = viewer.gui
+        
+        # Create window
+        window_l = gui.getWindowList()
+        if not windowName in window_l:
+            gui.windowID = gui.createWindow(windowName)
+        else:
+            gui.windowID = gui.getWindowID(windowName)
+                
+        # Create scene if needed
+        scene_l = gui.getSceneList()
+        if sceneName not in scene_l:
+            gui.createScene(sceneName)
+            gui.addSceneToWindow(sceneName, gui.windowID)
+
+        gui.sceneName = sceneName
+
+        return gui
+    
+    except:
+        print("Error while starting the viewer client. ")
+        print("Check wheter gepetto-viewer is properly started")
+
