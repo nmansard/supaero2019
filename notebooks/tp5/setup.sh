c737446754f183cadc14abe1e4123e510357c72b
Mansard <nmansard@laas.fr>
2019-02-20 21:25:14 +0100

    Move tp5 setup in tp5 folder.

---
+++ notebooks/tp5/setup.sh
@@ -0,0 +1,9 @@
+get pinocchio
+install cmake
+install libeigen3-dev
+cmake pinocchio without unittest
+add pinocchio in bashrc
+dowload crocoddul
+pip install scipy 
+apt install ffmpeg
+pip upgrade matplotlib
